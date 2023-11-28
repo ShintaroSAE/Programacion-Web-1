96e079ea3b1130089239958cb2ddd5ca7e5a5796
Eduardo Salazar Aguilar <91902283+ShintaroSAE@users.noreply.github.com>
2023-11-28 16:15:22 -0600

    Se añade la base de datos

---
+++ Proyecto/asistencia.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `usuarios` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nombre` text COLLATE utf8mb4_spanish2_ci NOT NULL,
+    `nombre_usuario` text COLLATE utf8mb4_spanish2_ci NOT NULL,
+    `no_cuenta` text COLLATE utf8mb4_spanish2_ci NOT NULL,
+    `direccion` text COLLATE utf8mb4_spanish2_ci NOT NULL,
+    `telefono` varchar(15) COLLATE utf8mb4_spanish2_ci NOT NULL,
+    `correo` text COLLATE utf8mb4_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
