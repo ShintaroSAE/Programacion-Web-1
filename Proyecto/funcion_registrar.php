e4f6e8d1f38f50cf0d2f3b8d88c5e788f4248224
Eduardo Salazar Aguilar <91902283+ShintaroSAE@users.noreply.github.com>
2023-11-28 16:02:55 -0600

    Se arreglan fallos de nombres

--- Proyecto/funcion_registrar.php
+++ Proyecto/funcion_registrar.php
@@ -13,7 +13,7 @@ $count = mysqli_num_rows($result);
 
 if($count ==1 ){
     echo'El nombre se usuario ya a sido ocupado';
-    header('Location: ./form_registro.php');
+    header('Location: ./formulario.php');
     
 }else{
     mysqli_query($conexion, "INSERT INTO usuarios (
@@ -34,7 +34,7 @@ if($count ==1 ){
 
 echo "<br />" . "<h2>" . "Usuario Creado Exitosamente!" . "</h2>";
 echo "<h4>" . "Bienvenido: " . $_POST['usuario'] . "</h4>" . "\n\n";
-echo "<h5>" . "<a href='./f.php'>Registro</a>" . "</h5>";
+echo "<h5>" . "<a href='./formulario.php'>Registro</a>" . "</h5>";
 
 //termina el else
 }
